2caaf935007f1e2444895e4be47bf9d092da8731
Medhanei  Abraha <abrahama2019@gmail.com>
2021-02-01 03:30:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board()
-    puts "#{[0]} | #{1]} | #{[2]}"
-end
 
-boards = ["5"," "," "]
 
-display_board(boards)
-
-def hi_name(name)
+def hi_name()
   puts "Hi my #{name}! and im a soccer player"
 end
 
